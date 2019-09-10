7f13ff762e0f1fd5a8e4c4ec2ae2721275a7ccf9
btaranenko <bogdan13taranenko@gmail.com>
2019-09-11 01:15:21 +0300

    change currency behaviour

--- src/CountryList/Constants.js
+++ src/CountryList/Constants.js
@@ -2,10 +2,13 @@ import { countries as rawCountries } from 'countries-list'
 export { default as SearchIcon } from './assets/images/SearchIcon/searchIcon.png';
 
 const formattedCountries = Object.keys(rawCountries)
-  .map(country => {
+  .map(countryCode => {
+    const country = rawCountries[countryCode]
+
     return {
-      ...rawCountries[country],
-      code: country
+      ...country,
+      currency: (country.currency && country.currency.includes(',')) ? country.currency.split(',')[0] : country.currency,
+      code: countryCode
     }
   })
   .sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
